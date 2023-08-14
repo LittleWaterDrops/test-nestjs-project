673d08f057b377f02145431cae5557d84ecff6ad
LittleWaterDrops <littleWaterDrops@github.com>
2023-08-14 22:04:11 +0900

    app controller 제어 시험 및 users restApi 생성

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,24 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('me')
+  getHelloMe(): string {
+    return this.appService.getHelloMe();
+  }
+}
+
+@Controller('yello')
+export class AppYelloController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getYello(): string {
+    return this.appService.getYello();
+  }
+
+  @Get('me')
+  getYelloMe(): string {
+    return this.appService.getYelloMe();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,13 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  getHelloMe(): string {
+    return 'Hello You!';
+  }
+  getYello(): string {
+    return 'Yello World!';
+  }
+  getYelloMe(): string {
+    return 'Yello You!';
+  }
 }
